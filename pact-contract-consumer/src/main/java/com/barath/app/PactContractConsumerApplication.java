3a800268ea828e9921cb08471c8f5227dfdc3d35
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-10-14 22:30:09 +0530

    FIRST COMMIT

---
+++ pact-contract-consumer/src/main/java/com/barath/app/PactContractConsumerApplication.java
@@ -0,0 +1,12 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactContractConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactContractConsumerApplication.class, args);
+	}
+}
---
+++ pact-contract-provider/src/main/java/com/barath/app/PactContractProviderApplication.java
@@ -0,0 +1,12 @@
+package com.barath.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PactContractProviderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PactContractProviderApplication.class, args);
+	}
+}
