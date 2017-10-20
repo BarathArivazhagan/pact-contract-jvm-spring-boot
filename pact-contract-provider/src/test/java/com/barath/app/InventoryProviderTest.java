2945096499e0427bd85cda9a85f2c98892e5f9d7
Barath Arivazhagan <barathmacdec91@gmail.com>
2017-10-20 23:41:35 +0530

    code commit

--- pact-contract-provider/src/test/java/com/barath/app/InventoryProviderTest.java
+++ pact-contract-provider/src/test/java/com/barath/app/InventoryProviderTest.java
@@ -1,5 +1,7 @@
 package com.barath.app;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -13,8 +15,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
@@ -29,33 +33,33 @@ import au.com.dius.pact.provider.junit.target.HttpTarget;
 import au.com.dius.pact.provider.junit.target.Target;
 import au.com.dius.pact.provider.junit.target.TestTarget;
 
-@RunWith(PactRunner.class)
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactBroker;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import au.com.dius.pact.provider.spring.SpringRestPactRunner;
+
+
+@RunWith(SpringRestPactRunner.class)
+@SpringBootTest(classes=PactContractProviderApplication.class,properties={"spring.profiles.active=test","spring.cloud.config.enabled=false"},webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@ActiveProfiles("test")
 @PactBroker(host="localhost",port="8500")
-@Provider("test_provider")
-public class SaveInventoryProviderTest {
+@Provider("inventory_provider")
+public class InventoryProviderTest {
 	
-	private static MockMvc mockMvc;
-	
-	private static ConfigurableApplicationContext context;
-	
-	private static final ObjectMapper mapper=new ObjectMapper();
-	
-  @BeforeClass
-  public static void setUpContext(){
-	  context= SpringApplication.run(PactContractProviderApplication.class);
-	  mockMvc=MockMvcBuilders.webAppContextSetup((WebApplicationContext) context).build();
-  }
-  
-  
+	@MockBean
+	private InventoryService inventoryService;
+
   @TestTarget
-  public final Target target = new HttpTarget(8080);
+  public final Target target = new HttpTarget(9050);
   
-  @State(value="a request to save inventory")
-  public void test() throws Exception{
+  @State(value="create inventory")
+  public void createInventoryState() throws Exception{
 	  
-	  Inventory inventory=new Inventory(1L, "TV", "CHENNAI", 100);
-	  mockMvc.perform(post("/api/inventory").content(mapper.writeValueAsString(inventory)).contentType(MediaType.APPLICATION_JSON_UTF8_VALUE)).andDo(print())
-	  		.andExpect(status().isOk())
-	  		.andExpect(jsonPath("$.productName").value("TV"));
+	  Inventory inventory=new Inventory("TV", "CHENNAI", 100);
+	  when(inventoryService.saveInventory(any(Inventory.class))).thenReturn(inventory) ;
   }
 }
